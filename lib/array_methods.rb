b95684bff6d5f43c132f9cdfe25026c6c7f40d8c
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 21:05:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,5 @@
 def using_include(array, element)
+<<<<<<< HEAD
 #  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 #  element1 = array.include?("wow")
 #  element2 = array.include?("Tardis")
@@ -6,21 +7,46 @@ def using_include(array, element)
 end
 
 def using_sort(array)
+=======
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.include?("wow")
+  array.include?("Tardis")
+end
+
+def using_sort(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+>>>>>>> f1880013dd287071ded645bac96ff2e0772b3da9
   array.sort
 end
 
 def using_reverse(array)
+<<<<<<< HEAD
+=======
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+>>>>>>> f1880013dd287071ded645bac96ff2e0772b3da9
   array.reverse
 end
 
 def using_first(array)
+<<<<<<< HEAD
+=======
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+>>>>>>> f1880013dd287071ded645bac96ff2e0772b3da9
   array.first
 end
 
 def using_last(array)
+<<<<<<< HEAD
+=======
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+>>>>>>> f1880013dd287071ded645bac96ff2e0772b3da9
   array.last
 end
 
 def using_size(array)
+<<<<<<< HEAD
+=======
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+>>>>>>> f1880013dd287071ded645bac96ff2e0772b3da9
   array.size
 end
